74e149388c1e46700c127499cda6c8bed3e699ad
Gunnar Skjold <gunnar@origin.no>
2021-04-10 11:11:16 +0200

    Updated build environment

--- scripts/makeweb.py
+++ scripts/makeweb.py
@@ -2,9 +2,34 @@ import os
 import re
 import shutil
 
+try:
+    from css_html_js_minify import html_minify, js_minify, css_minify
+except:
+    from SCons.Script import (
+        ARGUMENTS,
+        COMMAND_LINE_TARGETS,
+        DefaultEnvironment,
+    )
+    env = DefaultEnvironment()
+
+    env.Execute(
+        env.VerboseAction(
+            '$PYTHONEXE -m pip install "css_html_js_minify" ',
+            "Installing Python dependencies",
+        )
+    )
+    try:
+        from css_html_js_minify import html_minify, js_minify, css_minify
+    except:
+        print("WARN: Unable to load minifier")
+
+
 webroot = "web"
 srcroot = "src/web/root"
 
+version = os.environ.get('GITHUB_TAG')
+if version == None:
+    version = "SNAPSHOT"
 
 if os.path.exists(srcroot):
     shutil.rmtree(srcroot)
@@ -20,11 +45,28 @@ for filename in os.listdir(webroot):
 
     varname = basename.upper()
 
+    with open(srcfile, encoding="utf-8") as f:
+        content = f.read().replace("${version}", version)
+
+    try:    
+        if filename.endswith(".html"):
+            content = html_minify(content)
+        elif filename.endswith(".css"):
+            content = css_minify(content)
+        elif filename.endswith(".js") or filename.endswith(".json"):
+            content = js_minify(content)
+    except:
+        print("WARN: Unable to minify")
+
     with open(dstfile, "w") as dst:
         dst.write("const char ")
         dst.write(varname)
-        dst.write("[] PROGMEM = R\"==\"==(\n")
-        with open(srcfile, "r") as src:
-            for line in src.readlines():
-                dst.write(line)
-        dst.write("\n)==\"==\";\n")
+        dst.write("[] PROGMEM = R\"==\"==(")
+        dst.write(content)
+        dst.write(")==\"==\";\n")
+        dst.write("const int ");
+        dst.write(varname)
+        dst.write("_LEN PROGMEM = ");
+        dst.write(str(len(content)))
+        dst.write(";");
+        
