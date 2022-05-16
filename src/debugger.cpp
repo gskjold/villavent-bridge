81276ce76023ef13f152c9294ba732c45499a0e1
Sigurd Vindenes <sigvind@frisurf.no>
2022-05-16 20:53:15 +0200

    return values on printers

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -2,45 +2,49 @@
 #include "Arduino.h"
 
 size_t debugD(const char* format, ...) {
+    size_t cnt = 0;
     #if DEBUG_MODE
         va_list arg;
         va_start(arg, format);
-        char text[256];text[sizeof(text)-1]=0;
-	    vsnprintf(text, sizeof(text), format, arg);
-        Serial.printf("[DEBUG] %s\r\n", text);
+        char text[256]; text[sizeof(text)-1]=0;;
+	    vsnprintf(text, sizeof(text)-1, format, arg);
+        cnt = Serial.printf("[DEBUG] %s\r\n", text);
     #endif
-    return 0;
+    return cnt;
 }
 
 size_t debugI(const char* format, ...) {
+    size_t cnt = 0;
     #if DEBUG_MODE
         va_list arg;
         va_start(arg, format);
-        char text[256];text[sizeof(text)-1]=0;
+        char text[256]; text[sizeof(text)-1]=0;
 	    vsnprintf(text, sizeof(text), format, arg);
-        Serial.printf("[INFO] %s\r\n", text);
+        cnt =  Serial.printf("[INFO] %s\r\n", text);
     #endif
-    return 0;
+    return cnt;
 }
 
 size_t debugW(const char* format, ...) {
+    size_t cnt = 0;
     #if DEBUG_MODE
         va_list arg;
         va_start(arg, format);
-        char text[256];text[sizeof(text)-1]=0;
+        char text[256]; text[sizeof(text)-1]=0;
 	    vsnprintf(text, sizeof(text), format, arg);
-        Serial.printf("[WARNING] %s\r\n", text);
+        cnt =  Serial.printf("[WARNING] %s\r\n", text);
     #endif
-    return 0;
+    return cnt;
 }
 
 size_t debugE(const char* format, ...) {
+    size_t cnt = 0;
     #if DEBUG_MODE
         va_list arg;
         va_start(arg, format);
-        char text[256];text[sizeof(text)-1]=0;
+        char text[256]; text[sizeof(text)-1]=0;
 	    vsnprintf(text, sizeof(text), format, arg);
-        Serial.printf("[ERROR] %s\r\n", text);
+        cnt =  Serial.printf("[ERROR] %s\r\n", text);
     #endif
-    return 0;
+    return cnt;
 }
